e2a9b1b4332d0510b96fea553f6f08fb828014ee
gogoPAN <panpchen@gmail.com>
2021-03-17 18:23:40 +0800

    update Game.ts

--- assets/Script/Game.ts
+++ assets/Script/Game.ts
@@ -14,28 +14,18 @@ const CONFIG_TXT_LIST = [
   {
     txt:
       "庭院式组群布局，木结构建筑，屋檐形成如鸟翼伸展的檐角，又有隔离其他景色的的作用。",
-    color: "#FFA077",
-    txtColor: "#BC5022",
   },
   {
     txt: "树木对创造园林的气氛尤为重要，形成古朴幽深的意境。",
-    color: "#7AFF77",
-    txtColor: "#0E620C",
   },
   {
     txt: "多见于南方园林，增添园内景色，扩大空间。",
-    color: "#F7FF75",
-    txtColor: "#424515",
   },
   {
     txt: "无水不活，静态水景为主，以水为中心建造园景。",
-    color: "#97FFFD",
-    txtColor: "#0D605E",
   },
   {
     txt: "荷花。水与花卉、假山石相结合，体现园林的意境美。",
-    color: "#EBC5FF",
-    txtColor: "#74239F",
   },
 ];
 
@@ -53,8 +43,8 @@ export default class Game extends cc.Component {
   mainBg: cc.Node = null;
   private _curData = null;
   private _canStart: boolean = false;
+  private _isOver: boolean = false;
   private _remainCount: number = CONFIG_TXT_LIST.length;
-  private _lastTween: cc.Tween = null;
   onLoad() {
     this.printTitleEff(() => {
       this._canStart = true;
@@ -65,52 +55,43 @@ export default class Game extends cc.Component {
       "touchend",
       () => {
         this.onShowBubble(false);
-        this._lastTween = null;
-        this._remainCount--;
-        if (this._remainCount <= 0) {
-          // this.arrows.active = false;
-          this.bubble.active = false;
-          this.borders.active = false;
-
-          this.title.node.active = true;
-          this.title.node.scale = 0;
-          this.title.node.getChildByName("arrow").active = false;
-          this.title.string =
-            "恭喜你顺利通过了园林探索，中国古典园林追求 “诗情画意”的审美境界。将自然美和建筑美融合在一起，将山水、花木和亭台楼阁、厅堂廊树等巧妙地结合起来，形成了鲜明而独特的艺术风格。";
-          cc.tween(this.title.node)
-            .to(2, { scale: 1 }, { easing: "bounceOut" })
-            .start();
+        if (!this._isOver) {
+          this._remainCount--;
+          if (this._remainCount <= 0) {
+            this._isOver = true;
+            this.borders.active = false;
+            this.bubble.active = false;
+            this.title.node.active = true;
+            this.title.node.scale = 0;
+            this.title.node.getChildByName("arrow").active = false;
+            this._fadeOutMainBg();
+            this.title.string =
+              "恭喜你顺利通过了园林探索，中国古典园林追求 “诗情画意”的审美境界。将自然美和建筑美融合在一起，将山水、花木和亭台楼阁、厅堂廊树等巧妙地结合起来，形成了鲜明而独特的艺术风格。";
+            cc.tween(this.title.node)
+              .to(2, { scale: 1 }, { easing: "bounceOut" })
+              .delay(5)
+              .call(() => {
+                this.title.string = "";
+                this.borders.active = true;
+                this._fadeInMainBg();
+              })
+              .start();
+          }
         }
       },
       this
     );
 
-    // this.arrows.children.forEach((arrow) => {
-    //   cc.tween(arrow)
-    //     .repeatForever(
-    //       cc
-    //         .tween()
-    //         .parallel(
-    //           cc.tween().by(0.2, { y: 25 }),
-    //           cc.tween().to(0.2, { scale: 0.95 })
-    //         )
-    //         .parallel(
-    //           cc.tween().by(0.2, { y: -25 }),
-    //           cc.tween().to(0.2, { scale: 1 })
-    //         )
-    //         .delay(1)
-    //     )
-    //     .start();
-    // });
-
-    this.borders.children.forEach((border) => {
-      cc.tween(border)
-        .repeatForever(cc.tween().to(1, { scale: 0.95 }).to(1, { scale: 1 }))
+    this.arrows.children.forEach((arrow) => {
+      cc.tween(arrow)
+        .repeatForever(
+          cc.tween().by(0.2, { y: -25 }).delay(1).by(0.2, { y: 25 })
+        )
         .start();
     });
   }
 
-  printTitleEff(callback: Function) {
+  printTitleEff(callback: Function = null) {
     const charList = TITLE.split("");
     let id: number = 0;
     this.schedule(() => {
@@ -135,12 +116,11 @@ export default class Game extends cc.Component {
     this.title.node.active = false;
     this._fadeInMainBg(() => {
       this.scheduleOnce(() => {
-        // this.arrows.active = true;
-        this.borders.active = true;
+        this.arrows.active = true;
       }, 0.5);
     });
   }
-  _fadeInMainBg(callback: Function) {
+  _fadeInMainBg(callback: Function = null) {
     cc.tween(this.mainBg)
       .to(2, { color: cc.Color.WHITE })
       .call(() => {
@@ -149,45 +129,52 @@ export default class Game extends cc.Component {
       .start();
   }
 
-  onClickItem(evt, parm) {
-    // 初始是循环缩放效果,这里就先停止
-    if (this._lastTween) {
-      return;
-    }
-    cc.Tween.stopAllByTarget(evt.target);
-    const id = Number(parm);
-    // this.arrows.children[id].active = false;
-    this._curData = CONFIG_TXT_LIST[id];
-    this._lastTween = cc
-      .tween(evt.target)
-      .to(0.1, { scale: 1.1 })
-      .to(0.3, { scale: 0 })
-      .call(() => {
-        this.onShowBubble(true);
-      })
+  _fadeOutMainBg() {
+    cc.tween(this.mainBg)
+      .to(1, { color: new cc.Color().fromHEX("#737373") })
       .start();
   }
-  onShowBubble(enable: boolean) {
+
+  onClickItem(evt, parm) {
+    cc.Tween.stopAllByTarget(evt.target);
+    const id = Number(parm);
+    this.arrows.children[id].active = false;
+    this.borders.children[id].getComponent(cc.Sprite).enabled = true;
+    this._curData = CONFIG_TXT_LIST[id];
+    this.onShowBubble(true, id);
+  }
+
+  onShowBubble(enable: boolean, id: number = -1) {
     this.bubble.active = enable;
     if (enable) {
       if (this._curData) {
         cc.Tween.stopAllByTarget(this.bubble);
-        this.bubble.scale = 0;
-        const mask = this.bubble.getChildByName("mask");
-        mask.opacity = 0;
-        cc.tween(this.bubble)
-          .to(0.8, { scale: 1 }, { easing: "bounceOut" })
-          .call(() => {
-            mask.opacity = 170;
-          })
+        const bubbleBg = this.bubble.getChildByName("bg");
+        let pos = cc.Vec2.ZERO;
+        switch (id) {
+          case 0:
+            pos = cc.v2(223, -86);
+            break;
+          case 1:
+            pos = cc.v2(0, 0);
+            break;
+          case 2:
+            pos = cc.v2(254, 50);
+            break;
+          case 3:
+            pos = cc.v2(300, -56);
+            break;
+          case 4:
+            pos = cc.v2(342, -30);
+            break;
+        }
+        bubbleBg.setPosition(pos);
+        bubbleBg.scale = 0;
+        cc.tween(bubbleBg)
+          .to(0.8, { scale: 0.5 }, { easing: "bounceOut" })
           .start();
 
-        this.bubble.getChildByName("bg").color = cc
-          .color()
-          .fromHEX(this._curData.color);
-
-        const label = this.bubble.getChildByName("label");
-        label.color = cc.color().fromHEX(this._curData.txtColor);
+        const label = this.bubble.getChildByName("bg").getChildByName("label");
         label.getComponent(cc.Label).string = this._curData.txt;
       }
     }
